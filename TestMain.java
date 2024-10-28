d895a9cce3dfb638389f129d0e47b31b4f3eae86
Vy224 <vyntt.24ite@vku.udn.vn>
2024-10-28 08:15:33 +0700

    Add files via upload

--- TestMain.java
+++ TestMain.java
@@ -1,40 +1,23 @@
 public class TestMain {
     public static void main(String[] args) {
-        // Test constructor and toString()
-        Ball ball = new Ball(1.1f, 2.2f, 10, 3.3f, 4.4f);
-        System.out.println(ball);  // toString()
-
-        // Test Setters and Getters
-        ball.setX(80.0f);
-        ball.setY(35.0f);
-        ball.setRadius(5);
-        ball.setXDelta(4.0f);
-        ball.setYDelta(6.0f);
-        System.out.println(ball);  // toString()
-        System.out.println("x is: " + ball.getX());
-        System.out.println("y is: " + ball.getY());
-        System.out.println("radius is: " + ball.getRadius());
-        System.out.println("xDelta is: " + ball.getXDelta());
-        System.out.println("yDelta is: " + ball.getYDelta());
-
-        // Bounce the ball within the boundary
-        float xMin = 0.0f;
-        float xMax = 100.0f;
-        float yMin = 0.0f;
-        float yMax = 50.0f;
-        for (int i = 0; i < 15; i++) {
-            ball.move();
-            System.out.println(ball);
-            float xNew = ball.getX();
-            float yNew = ball.getY();
-            int radius = ball.getRadius();
-            // Check boundary value to bounce back
-            if ((xNew + radius) > xMax || (xNew - radius) < xMin) {
-                ball.reflectHorizontal();
-            }
-            if ((yNew + radius) > yMax || (yNew - radius) < yMin) {
-                ball.reflectVertical();
-            }
-        }
+        Time t1= new Time(1,2,3);
+        System.out.println(t1);
+        t1.setHour(4);
+        t1.setMinute(5);
+        t1.setSecond(6);
+        System.out.println(t1);
+        System.out.println( "Hour: " + t1.getHour());
+        System.out.println( "Minute: " + t1.getMinute());
+        System.out.println( "Second: " + t1.getSecond());
+        t1.setHour(23);
+        t1.setMinute(59);
+        t1.setSecond(58);
+        System.out.println(t1);
+        System.out.println(t1.nextSecond());
+        t1.nextSecond();
+        System.out.println(t1.nextSecond());
+        System.out.println(t1.previousSecond());
+        t1.previousSecond();
+        System.out.println(t1.previousSecond());
     }
 }
